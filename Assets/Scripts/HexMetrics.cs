795805c29838ea5ef20257c2220efac332fe406f
Timotius Prihadi <timotius@gamelevelone.com>
2017-04-06 18:24:17 +0700

    HexMap tutorial 1, before touching Cells

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexMetrics : MonoBehaviour {
+public static class HexMetrics {
+	public const float outerRadius = 10f;
+	public const float innerRadius = outerRadius * 0.866025404f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public static Vector3[] corners = {
+		new Vector3(0f,0f,outerRadius),
+		new Vector3(innerRadius,0f,0.5f*outerRadius),
+		new Vector3(innerRadius,0f,-0.5f*outerRadius),
+		new Vector3(0f,0f,-outerRadius),
+		new Vector3(-innerRadius,0f,-0.5f*outerRadius),
+		new Vector3(-innerRadius,0f,0.5f*outerRadius),
+		new Vector3(0f,0f,outerRadius),
+	};
+
 }
